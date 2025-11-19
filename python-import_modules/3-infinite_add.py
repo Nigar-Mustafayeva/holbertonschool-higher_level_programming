e1ec5c700271dd48f84694e85704a59b11350eef
Nigar-Mustafayeva <nigar.mustafzade112@gmail.com>
2025-11-19 10:33:14 +0000

    hfv

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 import sys
+
+
 def sum_command_line_arguments():
     total = 0
     for arg in sys.argv[1:]:
-            total += int(arg)
+        total += int(arg)
     print(f"{total}")
+
+
 if __name__ == "__main__":
     sum_command_line_arguments()
