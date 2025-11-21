482354bdc00f963927804f495c488add85ab4d8b
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-21 09:17:45 +0000

    jdc

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
+    first = snetence[0]
     if length == 0:
         return (0, None)
-    return (length, sentence[0])
+    return (length, first)
