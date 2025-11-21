a59c472cbee2f4afd5aac64a99bcbf649493363e
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-21 11:32:22 +0000

    dj

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -2,6 +2,6 @@
 def common_elements(set_1, set_2):
     c_set = set()
     for i in set_1:
-            if i in set_2:
-                c_set.add(i)
+        if i in set_2:
+            c_set.add(i)
     return c_set
