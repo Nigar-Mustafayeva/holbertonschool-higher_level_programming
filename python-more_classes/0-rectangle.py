4375cdcf0c3505ea0a07ebc2718a8b49b58825c1
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-25 10:16:48 +0000

    jnj

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Thsi class is used for Rectangle 
-
-This creates the simple class
-    which defines as Rectangle"""
+#Thsi class is used for Rectangle 
 class Rectangle:
+    #This code creates the Rectangle
     pass
