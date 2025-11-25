703410ee838235916935d5e8aa33c9f06ee8c9f8
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-25 10:23:22 +0000

    jnj

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-"""This class is used for Rectangle
-     and creates the empty class"""
+"""
+This class is used for Rectangle
+     and creates the empty class
+     """
+
 class Rectangle:
-    """This code creates the Rectangle
-    and adds the pass at the end of the script which is used to show that nothing to do"""
+    """
+    This code creates the Rectangle
+    and adds the pass at the end of the script which is used to show that nothing to do
+    """
     pass
