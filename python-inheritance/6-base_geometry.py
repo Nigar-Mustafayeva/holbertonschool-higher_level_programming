7465c47c458a87744b1d7d655170b9d167e0b0ff
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-28 13:09:10 +0000

    .

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is module documentation
+"""
+
+
+class BaseGeometry:
+    """
+    This is class documentation
+    """
+
+    def area(self):
+        raise NotImplementedError("area() is not implemented")
