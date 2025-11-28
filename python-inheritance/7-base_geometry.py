22ee4ef7d9d41f3ce5583fe27b31a44d93a44c61
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-28 13:35:26 +0000

    .

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is module documentation
+"""
+
+class BaseGeometry:
+    """
+    This is class documentation
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
