d692b5b49bd170a38989c93060bac5ef6ecc0af6
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-29 15:48:42 +0000

    .

---
+++ python-input_output/10-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Student module with to_json method that can filter attributes
+"""
+
+
+class Student:
+    """
+    Defines a student by first_name, last_name, and age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance.
+
+        Args:
+            first_name (str): First name of the student
+            last_name (str): Last name of the student
+            age (int): Age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns the dictionary representation of the Student instance.
+
+        Args:
+            attrs (list, optional): List of attribute names to include.
+                                    If None, returns all attributes.
+
+        Returns:
+            dict: Dictionary with selected or all attributes
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__.copy()
