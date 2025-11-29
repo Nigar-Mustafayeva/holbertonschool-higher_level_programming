c060438ad069bf0a57daa9801d0330e3ab8d0744
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-29 14:31:46 +0000

    .

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is module documentation
+"""
+
+def append_write(filename="", text=""):
+    """
+    This is function documentation
+    """
+
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.append(text)
