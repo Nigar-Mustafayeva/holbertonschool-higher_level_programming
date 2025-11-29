84937bfaaf25b28f1591f422ea6c40e5d1a4ffa6
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-29 15:02:36 +0000

    .

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is module documentation
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    this is function documentation
+    """
+
+    with open(filename, "w", encoding="utf-8") as f:
+        return json.load(f)
