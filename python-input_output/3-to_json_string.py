cc7f0083a0d9ba65f21f1ca7d6555a9ae0c2e81b
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-29 14:39:26 +0000

    .

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is module documentation
+"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """
+    This is function documentation
+    """
+
+    return json.dumps(my_obj)
