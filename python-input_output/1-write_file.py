b067014a00e0b8938d0e3fe95e0634b42fb45bd7
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-29 14:25:56 +0000

    .

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,4 +6,4 @@ This is module documentation
 
 def write_file(filename="", text=""):
     with open(filename, "w", encoding="utf-8") as f:
-        return f.write9text)
+        return f.write(text)
