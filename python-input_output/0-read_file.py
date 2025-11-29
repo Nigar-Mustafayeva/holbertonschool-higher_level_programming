70ee937439a3d7ad6ce757e3d5cc46c08cf922be
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-29 14:18:00 +0000

    .

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-This is module documentation
+Module that reads a text file and prints it to stdout.
 """
 
-with open("my_file_0.txt", "r", encoding="utf-8") as f:
-    content = f.read()
-    print(content)
+
+def read_file(filename=""):
+    """Reads a UTF-8 text file and prints its content to stdout."""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
