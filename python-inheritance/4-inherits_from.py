a6298d2d62606158918484c0d73a3ac887582625
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-30 17:44:18 +0000

    .

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     Return True if obj is an instance of a class that inherits
     (directly or indirectly) from a_class, otherwise False.
     """
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
