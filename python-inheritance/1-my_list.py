8299a03b663f2956f2fbcc25c9aef52d52885958
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-30 17:25:09 +0000

    .

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Module defining a MyList class that inherits from list."""
-
+# 1-my_list.py
 
 class MyList(list):
-    """MyList class that inherits from list."""
+    """MyList class that inherits from list"""
 
     def print_sorted(self):
-        """Print the list in ascending order without modifying original."""
+        """Prints the list in ascending order"""
         print(sorted(self))
