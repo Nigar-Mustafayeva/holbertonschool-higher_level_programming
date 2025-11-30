bc0bb4bb81c164b9a301463af96627c6bc33f814
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-30 14:32:44 +0000

    .

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Create user user_0d_1 if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on the server
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply changes immediately
+FLUSH PRIVILEGES;
