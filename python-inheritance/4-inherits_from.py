6569ca2d1ab9f1e94c5012b1416a544ac5a2f64a
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-30 17:43:20 +0000

    .

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Function that checks if an object inherits from a specified class."""
+"""
+Function that checks if an object inherits from a specified class.
+"""
 
 
 def inherits_from(obj, a_class):
@@ -7,5 +9,4 @@ def inherits_from(obj, a_class):
     Return True if obj is an instance of a class that inherits
     (directly or indirectly) from a_class, otherwise False.
     """
-
     return issubclass(type(obj), a_class) and type(obj) != a_class
