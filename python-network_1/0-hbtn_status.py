ef1e9d8a9448f406c62eeb51cb4e7322ac3a6048
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-12-01 17:25:01 +0000

    .

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This is documentation for module
+"""
+
+
 import urllib.request 
 req = urllib.request.Request('https://intranet.hbtn.io/status')
 with urllib.request.urlopen(req) as response:
